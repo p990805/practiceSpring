f69b8934626a73f933b002b6c21e045a92da6d34
박주찬 <p990805@naver.com>
2025-09-27 15:46:19 +0900

    feat : 스프링 입문 - 자바 코드로 직접 스프링 빈 등록하기

--- start-spring/hello/src/main/java/com/hello/hello/service/MemberService.java
+++ start-spring/hello/src/main/java/com/hello/hello/service/MemberService.java
@@ -9,10 +9,9 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+
 public class MemberService {
 
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
