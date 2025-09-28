316c3479f1c6bd9c45d7b8105b4b010b3886f457
박주찬 <p990805@naver.com>
2025-09-28 14:50:25 +0900

    feat : 스프링 입문 - H2, 순수 JDBC 이용

--- start-spring/hello/src/main/java/com/hello/hello/config/SpringConfig.java
+++ start-spring/hello/src/main/java/com/hello/hello/config/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.hello.hello.config;
 
+import com.hello.hello.repository.JdbcMemberRepository;
 import com.hello.hello.repository.MemberRepository;
 import com.hello.hello.repository.MemoryMemberRepository;
 import com.hello.hello.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
