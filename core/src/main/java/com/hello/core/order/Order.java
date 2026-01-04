c4d4cb35859728346c267152488ddf3801f46cf2
박주찬 <p990805@naver.com>
2026-01-05 00:23:49 +0900

    feat: 주문과 할인 도메인 실행과 테스트

---
+++ core/src/main/java/com/hello/core/order/Order.java
@@ -0,0 +1,29 @@
+package com.hello.core.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Order {
+
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+    public int calculatePrice() {
+        return itemPrice - discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+            "memberId=" + memberId +
+            ", itemName='" + itemName + '\'' +
+            ", itemPrice=" + itemPrice +
+            ", discountPrice=" + discountPrice +
+            '}';
+    }
+
+}
