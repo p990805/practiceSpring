3e6ed06d6db417f3b9b32f106a2cca082eb2f1c5
박주찬 <p990805@naver.com>
2026-01-12 15:26:28 +0900

    feat: 빈 등록 초기화, 소멸 메서드

---
+++ core/src/test/java/com/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,52 @@
+package com.hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect(){
+        System.out.println("connect: url = " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    public void disconnect(){
+        System.out.println("close: " + url);
+    }
+
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//
+//    @Override
+//    public void destroy() throws Exception {
+//        disconnect();
+//    }
+
+    public void init(){
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+
+}
